a9cacdaa68690df359a21e87ed23140b15204590
hyochan <dooboolab@gmail.com>
2021-01-19 07:59:44 +0900

    Added PWA supports

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,15 @@ import App from './App';
 
 AppRegistry.registerComponent('dooboolab', () => App);
 
-if (Platform.OS === 'web')
+if (Platform.OS === 'web') {
   AppRegistry.runApplication('dooboolab', {
     rootTag: document.getElementById('root'),
   });
+
+  if ('serviceWorker' in navigator)
+    navigator.serviceWorker.register(
+      `${process.env.PUBLIC_URL}/service-worker.js`,
+    );
+}
 
 export default App;
