748a75658a936640f7196dab46def53d726ecd40
hyochan <dooboolab@gmail.com>
2021-02-14 06:57:09 +0900

    Update `User` type

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,7 +3,14 @@ import {StyleProp, TextStyle} from 'react-native';
 import {SFC} from 'react';
 import firebase from 'firebase';
 
-export type User = firebase.User;
+export type User = {
+  uid: firebase.User['uid'];
+  email: firebase.User['email'];
+  displayName: firebase.User['displayName'];
+  emailVerified: firebase.User['emailVerified'];
+  photoURL: firebase.User['photoURL'];
+  introduction?: string;
+};
 
 interface IconProps {
   style?: StyleProp<TextStyle>;
