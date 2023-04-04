f93596c8ec3927d653b112cc99b8822fe32a5654
hyochan <dooboolab@gmail.com>
2023-04-04 22:23:55 +0900

    chore: facebook domain verification

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import Layout from '@theme/Layout';
 import Home from '../components/views/Home';
+import Head from '@docusaurus/Head';
 
 export default function Index(): JSX.Element {
   return (
-    <Layout>
+    <Layout title="dooboolab" description="dooboolab">
+      <Head>
+        {/* <meta property="og:image" content="image.png" /> */}
+        <meta
+          name="facebook-domain-verification"
+          content="w48rzhmmlek7a90w437qte9vl17oli"
+        />
+      </Head>
       <Home />
     </Layout>
   );
